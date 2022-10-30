c9a935c2ec773c9deaee46ef155a178a26c72598
Matt Turner <matt@fiverings.uk>
2022-10-30 19:03:02 +0000

    Update to use Github CLI

---
+++ README.md
@@ -0,0 +1,10 @@
+# Repo Manager
+
+Shows the list of repos matching the search criteria which have changed over a period of time.
+
+## Installation
+
+1. Copy `settings.sample.json` to `settings.json`
+1. Install Github CLI from [here](https://cli.github.com/manual/installation)
+2. Login to Github with `gh auth login`
+3. Run `./repo-man.ps1`
--- repo-man.ps1
+++ repo-man.ps1
@@ -1,32 +1,47 @@
-$token = Get-Content token.txt
+$settings = Get-Content "settings.json" | ConvertFrom-Json
+
+$owner_type = $settings.owner_type
+$owner = $settings.owner
+$search_term = $settings.search_term
+$date_format = $settings.date_format
+$duration = $settings.duration
+
+$date_since = (Get-Date).AddDays(-$duration)
+
+Write-Output "Searching for repos matching $search_term, pushed to since $($date_since.ToString($date_format)):"
+Write-Output ""
+
 $per_page = 100
-$base_url = "https://api.github.com/user"
-
-$date_since = (Get-Date).AddDays(-80)
-
-Write-Output $date_since
-
-$pat = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($token))
 
 $allRepos = @()
-$page = 0
+$page = 1
 do {
-    $page += 1    
-    $params = @{'Uri' = ('{0}/repos?page={1}&per_page={2}' -f
-            $base_url, $page, $per_page)
-        'Headers'     = @{'Authorization' = 'Basic ' + $pat }      
-        'Method'      = 'GET'                
-        'ContentType' = 'application/json'
-    }
-    $repos = Invoke-RestMethod @params   
+    $search_path = "/search/repositories?page={0}&per_page={1}&q=in:name+{2}+{3}%3A{4}" `
+        -f $page, $per_page, $search_term, $owner_type, $owner
+
+    $search_results = $(gh api $search_path) | ConvertFrom-Json
+
+    $repos = $search_results.Items
+
     $allRepos += $repos    
-    $repoCount = $repos.Count 
+    $repoCount = $repos.Count
+
+    $page += 1
+
 } while ($repoCount -gt 0)
 
+$recentRepos = @()
 foreach ( $repo in $allRepos ) {
     if ($repo.pushed_at -gt $date_since) {
-        Write-Output $repo
-        Write-Output $repo.name
-        Write-Output $repo.pushed_at
+        $recentRepos += $repo
     }
 }
+
+Write-Output "The following repos have been pushed to:"
+Write-Output ""
+
+foreach ( $repo in $recentRepos ) {
+    Write-Output "$($repo.name.PadRight(40)) $($repo.pushed_at.ToString($date_format))"
+}
+
+Write-Output ""
